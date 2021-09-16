7d42f5918d0c726bb27bec4ed0eff33cfc9005fe
FriendlyUser1 <kitty.kat5532@gmail.com>
2021-09-16 16:16:26 +0100

    fin

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
+function getRand() {
+	fetch("https://catfact.ninja/fact?max_length=512")
+		.then((response) => {
+			if (!response.ok)
+				return (document.getElementById("cat-fact").innerHTML =
+					"Whoops! There was an error.");
+			return response.json();
+		})
+		.then(
+			(data) => (document.getElementById("cat-fact").innerHTML = data.fact)
+		);
+}
 
+function getImg() {
+	fetch("https://api.thecatapi.com/v1/images/search")
+		.then((response) => {
+			if (!response.ok)
+				return document
+					.getElementById("cat-img")
+					.setAttribute("src", "./defaultCat.jpg");
+			return response.json();
+		})
+		.then((data) => {
+			if (data[0].height > 600) {
+				return getImg();
+			}
+			document
+				.getElementById("cat-img")
+				.setAttribute("src", data[0].url.toString());
+
+			getRand();
+		});
+}
